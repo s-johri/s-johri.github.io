da46bd10a9a75579fca7ce3e55bf407df334235b
Saurabh Johri <saurabhxj@gmail.com>
2023-04-13 19:36:50 +0530

    Handling touchscreen input

--- scripts/planetSwitch.js
+++ scripts/planetSwitch.js
@@ -1,5 +1,34 @@
-function changeImage() {
-    var image = document.getElementById('planet'); // Get the image element by its ID
-    var src = "/images/planet" + getRandomInt(1, 3) + '.png';
-    image.src = src;
-}
+// To avoid same planet on click
+var last = 1;
+
+document.addEventListener('DOMContentLoaded', function() {
+    var planet = document.getElementById('planetImage');
+
+    planet.addEventListener('click', function() {
+        var next = getRandomInt(1, 3);
+        while (next == last) {
+            var next = getRandomInt(1, 3); 
+        } 
+        last = next;
+        var src = "/images/planet" + next + '.png';
+        planet.src = src;
+        console.log(getRandomInt(1, 3)); 
+
+    }, false);
+
+    planet.addEventListener('touchstart', function(event) {
+        // Prevent the default behavior of touch event (e.g., scrolling)
+        event.preventDefault();
+        
+        // Perform the desired action on touch, such as changing the image source
+        var next = getRandomInt(1, 3);
+        while (next == last) {
+            var next = getRandomInt(1, 3); 
+        } 
+        last = next;
+
+        var src = "/images/planet" + next + '.png';
+        planet.src = src;
+
+    }, false);
+});
