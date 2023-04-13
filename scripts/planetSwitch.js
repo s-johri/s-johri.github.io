89212820a90a533fbd370d5875b19aa6140b091d
Saurabh Johri <saurabhxj@gmail.com>
2023-04-13 19:44:15 +0530

    Bug fixes

--- scripts/planetSwitch.js
+++ scripts/planetSwitch.js
@@ -5,22 +5,23 @@ document.addEventListener('DOMContentLoaded', function() {
     var planet = document.getElementById('planetImage');
 
     planet.addEventListener('click', function() {
+        // prevent choosing the same image
         var next = getRandomInt(1, 3);
         while (next == last) {
             var next = getRandomInt(1, 3); 
         } 
         last = next;
+
         var src = "/images/planet" + next + '.png';
         planet.src = src;
-        console.log(getRandomInt(1, 3)); 
 
-    }, false);
+    });
 
     planet.addEventListener('touchstart', function(event) {
         // Prevent the default behavior of touch event (e.g., scrolling)
         event.preventDefault();
         
-        // Perform the desired action on touch, such as changing the image source
+        // prevent choosing the same image
         var next = getRandomInt(1, 3);
         while (next == last) {
             var next = getRandomInt(1, 3); 
@@ -30,5 +31,5 @@ document.addEventListener('DOMContentLoaded', function() {
         var src = "/images/planet" + next + '.png';
         planet.src = src;
 
-    }, false);
+    });
 });
