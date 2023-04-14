ced20662905b239cc908b3fa5171d9edb452b7af
Saurabh Johri <saurabhxj@gmail.com>
2023-04-14 12:30:03 +0530

    Added UI for navigation

--- scripts/planetSwitch.js
+++ scripts/planetSwitch.js
@@ -1,35 +1,108 @@
 // To avoid same planet on click
-var last = 1;
+var page = 1;
 
 document.addEventListener('DOMContentLoaded', function() {
     var planet = document.getElementById('planetImage');
 
-    planet.addEventListener('click', function() {
-        // prevent choosing the same image
-        var next = getRandomInt(1, 3);
-        while (next == last) {
-            var next = getRandomInt(1, 3); 
-        } 
-        last = next;
+    var leftArrow = document.getElementById('leftKey');
+    var rightArrow = document.getElementById('rightKey');
+    var spaceKey = document.getElementById('spaceKey');
 
-        var src = "/images/planet" + next + '.png';
-        planet.src = src;
+    document.addEventListener('keydown', function(event){
+         
+        if (event.key == 'a' || event.key == 'ArrowLeft'){
+            page = page - 1 < 1 ? 1:page-1;
+
+            var src = "/images/planet" + page + '.png';
+            planet.src = src;
+
+            leftArrow.style.animationName = 'moveSpriteSheet';
+            leftArrow.style.animationDuration = '0.1s';
+            leftArrow.style.animationTimingFunction = 'steps(2)';
+
+            leftArrow.addEventListener('animationend', function() {
+                leftArrow.style.animationName = '';
+                leftArrow.style.animationDuration = '';
+                leftArrow.style.animationTimingFunction = '';
+            });
+        }
+        else if (event.key == 'd' || event.key == 'ArrowRight'){
+            page = page+1 > 3 ? 3 : page+1;
+
+            var src = "/images/planet" + page + '.png';
+            planet.src = src;
+
+            rightArrow.style.animationName = 'moveSpriteSheet';
+            rightArrow.style.animationDuration = '0.1s';
+            rightArrow.style.animationTimingFunction = 'steps(2)';
+
+            rightArrow.addEventListener('animationend', function() {
+                rightArrow.style.animationName = '';
+                rightArrow.style.animationDuration = '';
+                rightArrow.style.animationTimingFunction = '';
+            });
+        }
+        else if (event.key == ' ') {
+            spaceKey.style.animationName = 'moveSpriteSheet';
+            spaceKey.style.animationDuration = '0.1s';
+            spaceKey.style.animationTimingFunction = 'steps(2)';
+
+            spaceKey.addEventListener('animationend', function() {
+                spaceKey.style.animationName = '';
+                spaceKey.style.animationDuration = '';
+                spaceKey.style.animationTimingFunction = '';
+            });
+        }
 
     }, false);
 
-    planet.addEventListener('touchstart', function(event) {
-        // Prevent the default behavior of touch event (e.g., scrolling)
-        event.preventDefault();
-        
-        // prevent choosing the same image
-        var next = getRandomInt(1, 3);
-        while (next == last) {
-            var next = getRandomInt(1, 3); 
-        } 
-        last = next;
+    leftArrow.addEventListener('click', function(){
 
-        var src = "/images/planet" + next + '.png';
+        page = page - 1 < 1 ? 1:page-1;
+
+        var src = "/images/planet" + page + '.png';
         planet.src = src;
 
+        leftArrow.style.animationName = 'moveSpriteSheet';
+        leftArrow.style.animationDuration = '0.1s';
+        leftArrow.style.animationTimingFunction = 'steps(2)';
+
+        leftArrow.addEventListener('animationend', function() {
+            leftArrow.style.animationName = '';
+            leftArrow.style.animationDuration = '';
+            leftArrow.style.animationTimingFunction = '';
+        });
+
+    }, false);
+
+    rightArrow.addEventListener('click', function(){
+        
+        page = page+1 > 3 ? 3 : page+1;
+
+        var src = "/images/planet" + page + '.png';
+        planet.src = src;
+
+        rightArrow.style.animationName = 'moveSpriteSheet';
+        rightArrow.style.animationDuration = '0.1s';
+        rightArrow.style.animationTimingFunction = 'steps(2)';
+
+        rightArrow.addEventListener('animationend', function() {
+            rightArrow.style.animationName = '';
+            rightArrow.style.animationDuration = '';
+            rightArrow.style.animationTimingFunction = '';
+        });
+
+    }, false);
+
+    spaceKey.addEventListener('click', function(){
+        spaceKey.style.animationName = 'moveSpriteSheet';
+        spaceKey.style.animationDuration = '0.1s';
+        spaceKey.style.animationTimingFunction = 'steps(2)';
+
+        spaceKey.addEventListener('animationend', function() {
+            spaceKey.style.animationName = '';
+            spaceKey.style.animationDuration = '';
+            spaceKey.style.animationTimingFunction = '';
+        });
     }, false);
 });
